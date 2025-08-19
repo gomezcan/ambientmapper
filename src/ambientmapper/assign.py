d5e47e35b9bd9712ff842503f954679df52ce2ec
gomezcan <46493296+gomezcan@users.noreply.github.com>
2025-08-19 07:54:42 -0400

    Update assign.py
    assign.py patch to keep XAcount

--- src/ambientmapper/assign.py
+++ src/ambientmapper/assign.py
@@ -12,19 +12,23 @@ def assign_winners_for_chunk(filtered_dir: Path, chunk_file: Path, out_file: Pat
             sub = c[c["BC"].isin(bc)]
             if sub.empty: continue
             sub = sub.assign(Genome=genome)
-            rows.append(sub[["Read","BC","Genome","AS","MAPQ","NM"]])
-    big = pd.concat(rows, ignore_index=True) if rows else pd.DataFrame(columns=["Read","BC","Genome","AS","MAPQ","NM"])
+            rows.append(sub[["Read","BC","Genome","AS","MAPQ","NM","XAcount"]])
+            
+    big = pd.concat(rows, ignore_index=True) if rows else pd.DataFrame(
+        columns=["Read","BC","Genome","AS","MAPQ","NM", "XAcount"])
     if big.empty:
-        pd.DataFrame(columns=["Read","BC","Genome","AS","MAPQ","NM","delta_AS","is_tie"]).to_csv(out_file, sep="\t", index=False)
+        pd.DataFrame(columns=["Read","BC","Genome","AS","MAPQ","NM","XAcount","delta_AS"]).to_csv(out_file, sep="\t", index=False)
         return 0
-    big = big.sort_values(["Read","AS","MAPQ","NM","Genome"], ascending=[True,False,False,True,True])
+        
+    big = big.sort_values(["Read","AS","MAPQ","NM","Genome"], ascending=[True,False,False,True])
     top = big.groupby("Read", as_index=False).head(1).copy()
     nxt = (big.groupby("Read", as_index=False).nth(1)[["Read","AS","MAPQ","NM"]]
               .rename(columns={"AS":"AS2","MAPQ":"MAPQ2","NM":"NM2"}))
     top = top.merge(nxt, on="Read", how="left")
     top["delta_AS"] = top["AS"] - top["AS2"].fillna(-np.inf)
+    
     dup_key = big[["Read","AS","MAPQ","NM"]].duplicated(keep=False)
     tied = set(big.loc[dup_key,"Read"])
     top["is_tie"] = top["Read"].isin(tied)
-    top[["Read","BC","Genome","AS","MAPQ","NM","delta_AS","is_tie"]].to_csv(out_file, sep="\t", index=False)
+    top[["Read","BC","Genome","AS","MAPQ","NM", "XAcount","delta_AS","is_tie"]].to_csv(out_file, sep="\t", index=False)
     return int(len(top))
